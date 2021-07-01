9c99294aeb9ae907754b484f439a3e558c23f3d2
WiLGYSeF <37422580+WiLGYSeF@users.noreply.github.com>
2021-07-01 14:16:42 -0400

    add arg parsing and main()

--- bootanim.py
+++ bootanim.py
@@ -1,9 +1,47 @@
 #!/usr/bin/env python3
 
+import argparse
+import sys
+
 from bootanimation_class import BootAnimation
 
 
-#input_path = '../Amatsuka Uto Wink/bootanimation.zip'
-input_path = '../Amatsuka Uto Wink/'
-anim = BootAnimation(input_path)
-anim.save_gif('test.gif', load_time=10)
+def main(args):
+    parser = argparse.ArgumentParser(
+        description='convert android boot animations to gif previews',
+        usage='%(prog)s [options] INPUT_FILE_OR_DIR OUTPUT'
+    )
+    parser.add_argument('-v', '--verbose',
+        action='count', default=0,
+        help='verbose mode'
+    )
+    parser.add_argument('files',
+        action='store', metavar='PATH', nargs=2
+    )
+
+    group = parser.add_argument_group('gif options')
+    group.add_argument('--loop-limit',
+        action='store', metavar='COUNT', type=int, default=3,
+        help='set the loop count for a part meant to loop forever until boot (default 3)'
+    )
+    group.add_argument('--load-time',
+        action='store', metavar='TIME', type=float, default=0,
+        help='set a load time to simulate a phone finishing boot (seconds)'
+    )
+    group.add_argument('--loop-forever',
+        action='store_true', default=False,
+        help='the output gif will loop forever instead of just once'
+    )
+
+    argspace = parser.parse_args(args)
+
+    anim = BootAnimation(argspace.files[0])
+    anim.save_gif(
+        argspace.files[1],
+        loop_limit=argspace.loop_limit,
+        load_time=argspace.load_time,
+        loop_forever=argspace.loop_forever
+    )
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
